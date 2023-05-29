b0a7f47fb0b04249e77b01940d9c2d168b46a32a
Vitaliy <vitrl@yandex.ru>
2023-05-29 18:29:54 +0300

    add figures info

---
+++ triangle.cpp
@@ -0,0 +1,52 @@
+#include "triangle.h"
+
+#include <QPointF>
+#include <QTextStream>
+#include <algorithm>
+
+void Triangle::draw(QPainter &painter)
+{
+    painter.setBrush(brush);
+    painter.setPen(pen);
+    painter.drawPolygon(points.data(), points.size());
+}
+
+bool Triangle::isPointInside(const QPoint &P) const
+{
+
+    float Ax = points[0].x();
+    float Ay = points[0].y();
+
+    float Bx = points[1].x();
+    float By = points[1].y();
+
+    float Cx = points[2].x();
+    float Cy = points[2].y();
+
+    float Px = P.x();
+    float Py = P.y();
+
+    float w1 = (Ax * (Cy - Ay) + (Py - Ay) * (Cx - Ax) - Px * (Cy - Ay))
+            / ((By - Ay) * (Cx - Ax) - (Bx - Ax) * (Cy - Ay));
+
+    float w2 = (Py - Ay - w1 * (By - Ay)) / (Cy - Ay);
+
+    if (w1 >= 0 && w2 >= 0 && w1 + w2 < 1)
+        return true;
+    return false;
+}
+
+void Triangle::print()
+{
+    QTextStream out(stdout);
+
+    out << "Point1: (" << points[0].x() << "; " << points[0].y() << ")\n"
+        << "Point2: (" << points[1].x() << "; " << points[1].y() << ")\n"
+        << "Point3: (" << points[2].x() << "; " << points[2].y() << ")\n\n";
+}
+
+void Triangle::move(const QPoint &offset)
+{
+    for (auto &point : points)
+        point += offset;
+}
